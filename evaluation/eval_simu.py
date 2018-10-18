21bcbc2a173c81da334aed585df75d3f94ae4e03
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-10-18 18:51:24 +0200

    a first structure

---
+++ evaluation/eval_simu.py
@@ -0,0 +1,7 @@
+Le script d'évaluation eval_simu.xxx (R ou Python) prendra en entrée :
+un nom de répertoire d'entrée
+un nom de répertoire de sortie
+le nombre d'exécutions de la méthode à réaliser sur chaque fichier du jeu de données simulées
+(nommé n_runs ci après)
+Pour chaque fichier <identifiant_fichier_i.txt> du jeu de données simulées, un fichier
+<identifiant_fichier_i>_results.txt de n_runs mots dans {TP, FP, FN} sera généré.
