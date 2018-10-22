39b52031051dd286e64287285ff44def11024511
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-22 21:58:31 +0200

    Begin to think about parsing

---
+++ smmb_aco/include/parsing/parsing.hpp
@@ -0,0 +1,13 @@
+CSVParser<int> genos_csv(genos_file, separator, header);
+CSVParser<int> phenos_csv(phenos_file, separator, header);
+class parser
+{
+public:
+    //Constructor
+    parser()
+    //Destructor
+    //Use default?
+
+
+private:
+}
