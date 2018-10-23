80cca302241f0605ee1e4f82b738def1ecab5617
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-23 10:16:32 +0200

    ajout de la session teletype, rework architecture

--- smmb_aco/src/main.cpp
+++ smmb_aco/src/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-    // Data Importation
-    
+    //  DATA IMPORTATION
+        CSVParser<int> genos_csv(genos_file, separator, header);
+        CSVParser<int> phenos_csv(phenos_file, separator, header);
 }
