23b505366669e9b8ceedcea6f953e2e4396c9669
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-23 11:48:26 +0200

    work in progress

--- smmb_aco/src/smmb_aco/smmb_aco.cpp
+++ smmb_aco/src/smmb_aco/smmb_aco.cpp
@@ -5,7 +5,7 @@ n_it: nombre d'itération ACO
 n_ants: nombre de fourmis
 K: Taille du sous ensemble de variable echantilloné à partir de D, pour chaque fourmis
 n_it_n: nombre d'itération maximale qui force l'exploration de l'espace de recherche dans Markov Blanket
-alpha': seuil pour erreur de type I global
+global_alpha: seuil pour erreur de type I global
 Parametres liés a l'ACO:
 tau_0: valeur intiale pour un tax de phéromone de chaque variable
 rau, lambda: Deux constantes utilisees pour mettre a jour lers taux de phéromone
@@ -20,7 +20,7 @@ pour i allant de n_it
     pour a allant de 1 a n_ants //a parrelisé?
         D_a<- echantillonner(P, D, KI)
         mem_a <- ensemble_vide
-        MB_a <- learn_MB(D_a,T, k, N_it_n, alpha', mem_a, P)
+        MB_a <- learn_MB(D_a,T, k, N_it_n, global_alpha, mem_a, P)
     mem<-ensemble_vide
     pour a allant de 1 à n_ants
         ajouter(mem, mem_a)
@@ -40,11 +40,11 @@ function learn_MB()
         MB_modified <- false
         S<-echantillonné(P,D_a,k)
         s<-arg_max{score_association(s',T,MB_a,mem_a)} //l'argument qui maximise
-        si(p_valeur(s)<alpha')//cas de rejet de H_0
+        si(p_valeur(s)<global_alpha)//cas de rejet de H_0
         alors
             MB_a<-MB_a union s
             MB_modified <- true
-            backward_phase(MB_a, T, alpha')
+            backward_phase(MB_a, T, global_alpha)
         j++
     return MB_a
 }
@@ -54,7 +54,7 @@ procedure backward(MB, T, alpha)
     pour tout x element de MB
         pour toute combinaison S non_vides inclus dans MB
             realiser le terst d'independance entre X et T conditionnellement a S_0
-            Si p_valeur>alpha'//H_0: independance
+            Si p_valeur>global_alpha//H_0: independance
             alors
             MB <- MB\{x} //a gerer en liste
             break
@@ -62,29 +62,30 @@ procedure backward(MB, T, alpha)
 */
 #include <list>
 #include "smmb_aco.hpp"
-void learn_MB(std::list<unsigned> & mb, std::list<unsigned> & genotype_indexes)
+void learn_MB(genotype_matrix, phenotype_matrix, K, n_it_n, float global_alpha, mem_a, P)
 {
     MB_A = NULL;
     bool MB_modified = TRUE;
     int j = 0;
-    while (MB_modified or MB_a == NULL and j<<n_it_n) {
-        MB_modified = FALSE;
+    while (MB_modifie or MB_a == NULL and j<n_it_n)
+    {
+        MB_modifie = false
         /*
-        S<-echantillonné(P,D_a,k)
+        S = echantillone(P, D_a, k)
         s<-arg_max{score_association(s',T,MB_a,mem_a)} //l'argument qui maximise
         */
-        if (p_valeur(s) << alpha_prime) {//cas de rejet de H_0
+        if (p_valeur(s) << global_alpha) {//cas de rejet de H_0
             //MB_a<-MB_a union s
             MB_modified = TRUE;
-            backward_phase(MB_a, T, alpha_prime)
+            backward_phase(MB_a, T, global_alpha)
         }
     }
 }
-void forward(std::list<unsigned> & mb, std::list<unsigned> & genotype_indexes)
+void forward()
 {
 
 }
-void backward(std::list<unsigned> & mb, std::list<unsigned> & genotype_indexes)
+void backward(MB_a)
 {
     /* code */
 }
@@ -99,7 +100,7 @@ smmb_aco()
         for (size_t a = 0; a < n_ants; a++) { // a parallelise
             /* D_a<- echantillonner(P, D, KI)
             mem_a <- ensemble_vide
-            MB_a <- learn_MB(D_a,T, k, N_it_n, alpha', mem_a, P) */
+            MB_a <- learn_MB(D_a,T, k, N_it_n, global_alpha, mem_a, P) */
         }
         //quel type?? mem = NULL
         for (size_t a = 0; a < n_ants; a++) {
