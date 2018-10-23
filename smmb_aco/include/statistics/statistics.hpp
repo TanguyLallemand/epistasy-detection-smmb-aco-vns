942e438a4bcd09c562371bc95e00aa8112888247
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-23 21:40:15 +0200

    begin conditionnal independance test

---
+++ smmb_aco/include/statistics/statistics.hpp
@@ -0,0 +1,5 @@
+#include "G2_test_indep.hpp"
+#include "Contingency.hpp"
+
+#include <vector>
+#include <list>
---
+++ smmb_aco/src/statistics/statistics.cpp
@@ -0,0 +1,5 @@
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/math/distributions/chi_squared.hpp>
+#include <iostream>
+
+#include "statistics.hpp"
