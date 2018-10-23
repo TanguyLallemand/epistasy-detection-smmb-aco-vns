713945e9fd21883105a230a68fcd0a349f771e53
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-23 12:11:11 +0200

    update functions

--- smmb_aco/include/smmb_aco/smmb_aco.hpp
+++ smmb_aco/include/smmb_aco/smmb_aco.hpp
@@ -2,7 +2,7 @@
 class smmb_aco
 {
 public:
-void learn_MB(std::list<unsigned> & mb, std::list<unsigned> & genotype_indexes);
+void learn_MB(genotype_matrix, phenotype_matrix, int K, int n_it_n, float global_alpha, mem_a, P);
 void forward(std::list<unsigned> & mb, std::list<unsigned> & genotype_indexes);
 void backward(std::list<unsigned> & mb, std::list<unsigned> & genotype_indexes);
 private:
