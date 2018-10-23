8d0f86244f57c99e230f1773f99b43cb4d4657f2
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-23 20:55:03 +0200

    generation of genotype_dataset functionnal and commented

--- generate_toy_dataset.py
+++ generate_toy_dataset.py
@@ -9,13 +9,19 @@ import random
 import string
 
 
+# Function to generate ID using characters and digits
 def random_id(size):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
 
+# intitialisation array of ID
 genotype_id = []
+# Loop to get 28 ID
 for i in range(28):
     genotype_id.append(random_id(2))
+# Generate dataset
 genotype_dataset = np.random.randint(3, size=(4000, 28))
-np.savetxt("./toy_dataset/genotype_toy_dataset.txt",genotype_dataset, fmt='%i')
-np.savetxt("./toy_dataset/genotype_toy_dataset.txt", genotype_id, "%s")
+
+# Concatenate both arrays and print it as a csv file
+np.savetxt("./toy_dataset/genotype_toy_dataset.txt",
+           np.r_[[genotype_id], genotype_dataset], fmt='%s', delimiter=',')
