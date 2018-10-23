be594e3f6f7d3f50f1895a240187243605586e91
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-23 19:26:14 +0200

    begin generation of toy dataset

--- generate_toy_dataset.py
+++ generate_toy_dataset.py
@@ -2,3 +2,20 @@
 # -*- coding: utf-8 -*-
 # Authors: Tanguy Lallemand M2 BB
 #          Jonathan Cruard M BB
+
+import numpy as np
+import pandas as pd
+import random
+import string
+
+
+def random_id(size):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+
+genotype_id = []
+for i in range(28):
+    genotype_id.append(random_id(2))
+genotype_dataset = np.random.randint(3, size=(4000, 28))
+np.savetxt("./toy_dataset/genotype_toy_dataset.txt",genotype_dataset, fmt='%i')
+np.savetxt("./toy_dataset/genotype_toy_dataset.txt", genotype_id, "%s")
