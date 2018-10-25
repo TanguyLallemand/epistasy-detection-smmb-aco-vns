7cd1293daa9883a13828708128de0db67c9ee22b
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-25 22:15:59 +0200

    add principe algo and some init

--- smmb_aco/src/main.cpp
+++ smmb_aco/src/main.cpp
@@ -8,7 +8,26 @@
 
 int main(int argc, char* argv[])
 {
+    // Arguments
+    string genos_file = argv[1];
+    string phenos_file = argv[2];
+    
     parameters_parsing params;
     params.list_parameters();
 
+    params.genos_file = genos_file;
+    params.phenos_file = phenos_file;
+    int header = params.header;
+    char separator = params.separator;
+    //get_datas(string file_genotype, string file_phenotype)
+    smmb_aco.run();//exemple de call de la méthode smmb
+
+
+/*Algo du main
+
+parser les parametres
+parser les fichiers et les charger en tant que matrice
+les passer a smmb_ACO
+ecrire les resultats
+*/
 }
