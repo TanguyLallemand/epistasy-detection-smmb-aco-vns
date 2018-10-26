7704105edc571b52d3efd292d70d27844758e2ce
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-10-26 15:01:47 +0200

    more debugging

--- smmb_aco/include/smmb_aco/smmb_aco.hpp
+++ smmb_aco/include/smmb_aco/smmb_aco.hpp
@@ -25,14 +25,15 @@ public:
     alpha, beta: Deux constantes utilisees pour ajuster les poids respectifs entre les taux de phéromones et les connaissances a priori
     */
 // Constructeur
-    smmb_aco(boost::numeric::ublas::matrix<int> genotype_matrix, boost::numeric::ublas::matrix<int> phenotype_matrix, int n_it, int n_ants, int K, int n_it_n, double alpha, int tau_0, int rau, int tau, int eta, int alpha_stat, int beta);
-    void learn_MB(boost::numeric::ublas::matrix<int> genotype_matrix, boost::numeric::ublas::matrix<int> phenotype_matrix, int K, int n_it_n, double alpha, list<unsigned> mem_a/*, P*/);
-    void forward(bool MB_modifie, list<unsigned> MB_a, int n_it_n, int j/*, P*/, boost::numeric::ublas::matrix<int> genotype_matrix, int K );
+    smmb_aco(boost::numeric::ublas::matrix<int> genotype_matrix, boost::numeric::ublas::matrix<int> phenotype_matrix, size_t n_it, size_t n_ants, int K, size_t n_it_n, double alpha, int tau_0, int rau, int tau, int eta, int alpha_stat, int beta);
+    void smmb_aco::learn_MB(boost::numeric::ublas::matrix<int> genotype_matrix, boost::numeric::ublas::matrix<int> phenotype_matrix, int K, size_t n_it_n, double alpha, int mem_a/*, P*/);
+    void forward(bool MB_modifie, list<unsigned> MB_a, size_t n_it_n, int j/*, P*/, boost::numeric::ublas::matrix<int> genotype_matrix, int K );
     void backward(list<unsigned> MB_a, boost::numeric::ublas::matrix<int> phenotype_matrix, double alpha);
-    void run();
+    void run(boost::numeric::ublas::matrix<int> genotype_matrix,int T, int K, size_t n_it_n, size_t n_ants);
 
 private:
-
+    float p_valeur;
+    int s;
     int mem_a;
     // mettre les types     P;
     double alpha;
--- smmb_aco/src/smmb_aco/smmb_aco.cpp
+++ smmb_aco/src/smmb_aco/smmb_aco.cpp
@@ -72,10 +72,10 @@ procedure backward(MB, T, alpha)
 // smmb_aco : learn_MB
 //=================================================
 //Return Markov Blanket sous optimale eventuellemenet vide
-void smmb_aco::learn_MB(boost::numeric::ublas::matrix<int> genotype_matrix, boost::numeric::ublas::matrix<int> phenotype_matrix, int K, int n_it_n, double alpha, int mem_a/*, P*/)
+void smmb_aco::learn_MB(boost::numeric::ublas::matrix<int> genotype_matrix, boost::numeric::ublas::matrix<int> phenotype_matrix, int K, size_t n_it_n, double alpha, int mem_a/*, P*/)
 {
-    list<unsigned> MB_A = NULL;
-    bool MB_modified = TRUE;
+    list<unsigned> MB_a;
+    bool MB_modified = true;
     int j = 0;
     forward();
     return MB_a;
@@ -83,21 +83,21 @@ void smmb_aco::learn_MB(boost::numeric::ublas::matrix<int> genotype_matrix, boos
 //=================================================
 // smmb_aco : forward
 //=================================================
-void smmb_aco::forward(bool MB_modified, list<unsigned> MB_a, int n_it_n, int j/*, P*/, boost::numeric::ublas::matrix<int> genotype_matrix, int K )
+void smmb_aco::forward(bool MB_modified, list<unsigned> MB_a, size_t n_it_n, int j/*, P*/, boost::numeric::ublas::matrix<int> genotype_matrix, int K )
 {
-    while (MB_modified or MB_a == NULL and j<n_it_n)
+    while (MB_modified || (MB_a.empty() && j<n_it_n))
     {
         MB_modified = false;
         /*
         S = echantillone(P, genotype_matrix, k)
         s<-arg_max{score_association(s',T,MB_a,mem_a)} //l'argument qui maximise
         */
-        if (p_valeur(s) << alpha)
-        {//cas de rejet de H_0
-            MB_a = std::set_union (MB_A, MB_A + MB_A.size(), S, S+S.size(), v.begin());// union de MB_A et S et retourne avec v.begin le debut du vecteur MB_A
-            MB_modified = TRUE;
+        //if (p_valeur(s) << alpha) //TODO: Il faut une fonction pour calculer/renvoyer la p_valeur de la solution
+        //{//cas de rejet de H_0
+            MB_a = std::set_union (MB_a, MB_a + MB_a.size(), S, S+S.size(), MB_a.begin());// union de MB_a et S et retourne avec v.begin le debut du vecteur MB_a
+            MB_modified = true;
             backward(MB_a, T, alpha);
-        }
+        //}
         j++;
     }
 }
@@ -108,7 +108,7 @@ void smmb_aco::backward(list<unsigned> MB_a, boost::numeric::ublas::matrix<int> 
 {
     for (size_t X = 0; X < MB.size; X++) {
         for (size_t S = 0; S < count; S++) {
-            independance_test_conditionnal(X,T,S_0);
+            //independance_test_conditionnal(X,T,S_0); //TODO: omg c est chaud ca
             if (p_valeur>alpha) { //H_0: independance
                 // MB <- MB\{x}; //a gerer en liste
                 break;
@@ -119,10 +119,10 @@ void smmb_aco::backward(list<unsigned> MB_a, boost::numeric::ublas::matrix<int> 
 //=================================================
 // smmb_aco : run
 //=================================================
-void smmb_aco::run()
+void smmb_aco::run(boost::numeric::ublas::matrix<int> genotype_matrix,int T, int K, size_t n_it_n, size_t n_ants)
 {
     // Initialization of Markov Blanket
-    list<unsigned> MB_S = NULL;
+    list<unsigned> MB_S;
     float tau = tau_0;
     for (size_t i = 0; i < n_iteration; i++)
     {
@@ -132,15 +132,15 @@ void smmb_aco::run()
         { // a parallelise
             // genotype_matrix<- echantillonner(P, D, KI)
             // Initialization of memory
-            list<unsigned> mem_a = NULL;
+            list<unsigned> mem_a;
             // Generate Markov Blanket and stock it in a temp variable
             list<unsigned> MB_a = learn_MB(genotype_matrix,T, K, n_it_n, alpha, mem_a/*, P*/);
         }
         // Initialization of final variable
-        list<unsigned> mem = NULL
+        list<unsigned> mem;
         for (size_t a = 0; a < n_ants; a++) {
             //ajouter(mem, mem_a);
-            if (MB_a != NULL) {
+            if (!MB_a.empty()) {
                 MB_S = MB_S + MB_a;
             }
             //post traitement;
