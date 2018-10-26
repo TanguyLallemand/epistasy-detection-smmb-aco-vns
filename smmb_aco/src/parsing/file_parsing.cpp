9dd0f711f34ab86af67f80920e29cd0bea1272f7
JonathanCruard <jonathan.cruard@gmail.com>
2018-10-26 11:54:10 +0200

    debug

--- smmb_aco/src/parsing/file_parsing.cpp
+++ smmb_aco/src/parsing/file_parsing.cpp
@@ -7,24 +7,6 @@ using namespace std;
 
 //TODO voir si un destructeur est nécessaire
 
-class data_parsing
-{
-public:
-    data_parsing(string filename, int header_size, char separator);
-    void initialise_empty_matrix();
-    void data_to_matrix();
-    void get_col_nb(string _file_name);
-    void get_line_nb(string _file_name);
-    boost::numeric::ublas::matrix<int> return_matrix();
-
-private:
-    string _file_name;
-    int _header_size;
-    int _row_number;
-    int _col_number;
-    char _separator;
-    boost::numeric::ublas::matrix<int> _matrix;
-}
 
 //=================================================
 // data_parsing : //TODO constructeur ici
