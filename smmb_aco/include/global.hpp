713fe9807139ac33a33a283e2c47f3e5700b3816
JonathanCruard <jonathan.cruard@gmail.com>
2018-10-29 14:50:27 +0100

    include boost vector

--- smmb_aco/include/global.hpp
+++ smmb_aco/include/global.hpp
@@ -2,6 +2,7 @@
 #define GLOBAL_HPP
 
 #include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
 typedef boost::numeric::ublas::matrix<int> boost_matrix;
 typedef boost::numeric::ublas::vector<float> boost_vector;
 #endif
