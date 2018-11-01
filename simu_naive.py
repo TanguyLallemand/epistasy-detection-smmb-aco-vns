17eb231dfdda6c1ee78f65d5d73b6c5016b8e25a
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-11-01 16:06:33 +0100

    add simu_naive file that will generate dataset

---
+++ simu_naive.py
@@ -0,0 +1,8 @@
+# Votre logiciel de simulation simu_naive.xxx prendra en entrée :
+# un nom de répertoire de sortie
+# le nombre de fichiers à générer
+# le préfixe commun aux identifiants des fichiers
+# le nombre de variables à simuler
+# le nombre de cas à simuler
+# le nombre de contrôles à simuler
+# Vous fournirez un script Python launch_simu_naiv
