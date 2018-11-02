356b154ce00f7703ef7d307b1ff292ddd4065557
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-11-02 17:06:27 +0100

    add many comment from sinoquet's class

--- smmb_aco/include/smmb_aco/smmb_aco.hpp
+++ smmb_aco/include/smmb_aco/smmb_aco.hpp
@@ -27,15 +27,17 @@ private:
     //variables initialisée par le constructeur à partir de params
     int _n_it; // nombre d'itérations ACO
     int _n_ant; // nb de fourmis
-    int _subset_size; // taille du subset créé par chaque fourmis
+    int _subset_size; // taille du subset de variables echantillonnees à partir de _genos_matrix pour chaque fourmis (on echantillone donc les SNPs pas les individus)
+    int ; //TODO nomme le comme tu veux elle c'est k //taille d'une combinaison de variables echantillonnées parmi _subset_size
     int _n_it_n; // nombre d'itération maximales pour explorer l'espace de recherche
     double _alpha_stat; // seuil de significativité
-    double _tau_0; // valeur de phéromone initiale
-    double _alpha_phero; // ces 2 variables influencent l'ajustement du taux de pheromones
-    double _beta_phero;
+    double _tau_0; // valeur initiale de phéromone de chaque variable, au debut des temps egalite parfaite car on a pas de connaissance. A traiter comme un vecteur
+    //Mise a jour du taux de phéromones
     double _rho; // taux d'évaporation
     double _lambda; // values used in evaporation rates updates
-    boost_vector _eta; // vecteur de poids apriori a ajouter à tau, vector of weights (whose size is the nunmber of variables), to account for prior knowledge on the variables
+    boost_vector _eta; // vecteur de poids indiquant des connaissances a priori pour les variables. A traiter comme un vecteur. Car on a peut etre une idee sur certain SNP, ces connaissances viendraient de publis, si on ne sais rien on met eta en uniforme. eta pourrait tres bien etre un fichier texte avec des connaissances a priori qui serait vectorisé
+    double _alpha_phero; //TODO peut etre a rennomer // Deux constantes utilisées pour ajuster les poids respectifs entre les taux de phéromones et les connaissances a priori. Y a peut etre des SNPs qu on connait et donc on lui donne une bonne note. Ca permet donc de regler le cursuer entre importance des phéromones et importance des connaissances a priori
+    double _beta_phero;
 
     //variables modifiées pendant le run
     boost_vector _tau;//tau doit etre un vecteur de la taille du nombre de SNP
