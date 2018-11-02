9204a61e5cb0e99f156da31dd71c1f6ea27ada88
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-11-02 11:44:47 +0100

    remove useless comments

--- simu_naive.py
+++ simu_naive.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# Does not need any shebang because used in a virtual environment
 
 # -*- coding: utf-8 -*-
 # Authors: Tanguy Lallemand M2BB
@@ -150,12 +149,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Votre logiciel de simulation simu_naive.xxx prendra en entrée :
-# un nom de répertoire de sortie
-# le nombre de fichiers à générer
-# le préfixe commun aux identifiants des fichiers
-# le nombre de variables à simuler
-# le nombre de cas à simuler
-# le nombre de contrôles à simuler
-# Vous fournirez un script Python launch_simu_naiv
