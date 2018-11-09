9e78023d22b68d6e64a22e5bc80f05903e778c1f
JonathanCruard <jonathan.cruard@gmail.com>
2018-11-09 17:20:15 +0100

    boost_matrix_float typedef

--- smmb_aco/include/global.hpp
+++ smmb_aco/include/global.hpp
@@ -6,6 +6,7 @@
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <string>
 typedef boost::numeric::ublas::matrix<unsigned int> boost_matrix;
+typedef boost::numeric::ublas::matrix<float> boost_matrix_float;
 typedef boost::numeric::ublas::vector<float> boost_vector_float;
 typedef boost::numeric::ublas::vector<int> boost_vector_int;
 typedef boost::numeric::ublas::vector<std::string> boost_vector_string;
