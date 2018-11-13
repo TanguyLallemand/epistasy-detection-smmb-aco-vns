5d5b94b098e9ccdd02dae6421e5a6580f5adb7df
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-13 20:52:43 +0100

    add contigencies class

---
+++ smmb_aco/include/statistics/contigencies.hpp
@@ -0,0 +1,13 @@
+/*
+Authors: Tanguy Lallemand M2BB
+         Jonathan Cruard M2BB
+*/
+#ifndef CONTIGENCIES_HPP
+#define CONTIGENCIES_HPP
+
+class contingencies
+{
+public:
+private:
+};
+#endif
---
+++ smmb_aco/src/statistics/contigencies.cpp
@@ -0,0 +1,9 @@
+/*
+   Authors: Tanguy Lallemand M2BB
+         Jonathan Cruard M2BB
+ */
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/math/distributions/chi_squared.hpp>
+#include <iostream>
+
+#include "statistics.hpp"
