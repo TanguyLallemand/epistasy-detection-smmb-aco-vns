683b49ec5401e4a3d7d5d2ba2b411e16f9da86a3
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-14 23:17:15 +0100

    change proto

--- smmb_aco/include/statistics/statistics.hpp
+++ smmb_aco/include/statistics/statistics.hpp
@@ -23,7 +23,7 @@ public:
     static float compute_p_value(boost_matrix const& _genos_matrix, boost_vector_int const& _phenos_vector);
     static float compute_chi_2(boost_matrix_float const& contingency_table, boost_matrix_float const& contingency_theorical_table);
     static unsigned int compute_liberty_degree(boost_matrix_float const& contingency_table);
-    static float make_contingencies_chi_2_conditional_test_indep(boost::numeric::ublas::matrix_column<boost::numeric::ublas::matrix<int>> const& _genos_column, boost::numeric::ublas::matrix_column<boost::numeric::ublas::matrix<int>> const& _phenos_column, std::list<unsigned> const& cond_genos_indexes);
+    static float make_contingencies_chi_2_conditional_test_indep(boost::numeric::ublas::matrix_column<boost::numeric::ublas::matrix<int>> const& _genos_column, boost_vector_int const& _phenos_vector, std::list<unsigned> const& cond_genos_indexes);
     //static float compute_chi_2_conditional_test_indep(boost::numeric::ublas::vector<contingencies contingencies(2,3)> const& contingencies_vector);
 
 
