bcba29bbfb0ddb194efb4d9c765e7ed6fd7be520
JonathanCruard <jonathan.cruard@gmail.com>
2018-11-20 19:47:08 +0100

    conflict

--- smmb_aco/include/tools/tools.hpp
+++ smmb_aco/include/tools/tools.hpp
@@ -5,11 +5,7 @@
 class tools
 {
 public:
-<<<<<<< HEAD
     static boost::numeric::ublas::vector<int> sampling(int subset_size, boost::numeric::ublas::vector<float> weight_vector, std::mt19937 rng);
-=======
-    static boost::numeric::ublas::vector<int> sampling(unsigned subset_size, boost::numeric::ublas::vector<float> weight_vector);
->>>>>>> 173d5fd185c4d44d4d63415ff6441d6a60be65fa
 };
 
 #endif
