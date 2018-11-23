902f4dc12ec23d928ea0480fe4506159f69e9ff5
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-11-23 11:19:26 +0100

    final cut for me

--- simu_naive.py
+++ simu_naive.py
@@ -11,9 +11,9 @@ import string
 from math import exp
 
 
-###############################################################################
-# Function to define arguments for script
-###############################################################################
+################################################################################
+# Function to define arguments for script                                      #
+################################################################################
 
 
 def get_arguments():
@@ -63,9 +63,9 @@ def get_arguments():
     # Return parsed args
     return args
 
-###############################################################################
-# This function permit to create an output directory if it does not exist
-###############################################################################
+################################################################################
+# This function permit to create an output directory if it does not exist      #
+################################################################################
 
 
 def check_output_directory(output_directory):
@@ -86,54 +86,70 @@ def check_output_directory(output_directory):
         else:
             raise
 
-###############################################################################
-# Generation of causal genotype dataset
-###############################################################################
+################################################################################
+# Generation of causal genotype dataset                                        #
+################################################################################
 
 
 def generate_genotype_with_linear_regression(number_of_patient, pattern_size):
+    # Initialisation of some variables
+    # Variable to store mean
     mean = 0
+    # List of possible value for beta
     array_of_beta = [-0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -
                      0.8, -0.9, -1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    # List of possible value in genotype matrix: 0: absence of SNP,
+    # 1: SNP heterozygote, 2: SNP homozygote
     possible_values = [0, 1, 2]
-
+    print("############################ Generating causal SNPs ############################")
+    # Loop for generate right values
     while mean != 0.5:
-        print(mean)
+        # Initialisation of temporary lists
         associated_phenotype = []
         genotype_linearly_generated_dataset = {}
+        # For every values to generate
         for i in range(0, number_of_patient):
+            # Initialisation of list to store temporary values
             x_list = []
             list_of_beta = []
             y_list = []
+            # For every causal SNPs to generate
             for j in range(pattern_size):
-                k = random.choice(possible_values)
-                x_list.append(k)
+                # Generate a value to fill matrix
+                x_list.append(random.choice(possible_values))
             for j in range(pattern_size):
-                k = random.choice(array_of_beta)
-                list_of_beta.append(k)
+                # Choose a beta
+                list_of_beta.append(random.choice(array_of_beta))
             for j in range(0, pattern_size):
-                y = (list_of_beta[j]) * (x_list[j])
-                y_list.append(y)
+                # Calculate y of every generated tuple matrix value and associated beta
+                y_list.append((list_of_beta[j]) * (x_list[j]))
+            # Sum all y calculated
             y = sum(y_list)
-            p = 1 / (1 + exp(-y))
-            if p >= 0.5:
+            # Calcluate precision to determine phenotype
+            precision = 1 / (1 + exp(-y))
+            # If precision is greater than 0.5 patient is sick
+            if precision >= 0.5:
                 associated_phenotype.append(1)
-            elif p < 0.5:
+            # If precision is lesser than 0.5 patient is a control
+            elif precision < 0.5:
                 associated_phenotype.append(0)
+            # Stor generated values
             genotype_linearly_generated_dataset[i] = x_list
+        #Calculate mean to get 50% of sick
         mean = (sum(associated_phenotype) / number_of_patient)
         # Return genotype dataset and associated phenotype
     return [list(genotype_linearly_generated_dataset.values()), associated_phenotype]
 
-###############################################################################
-# Generation of genotype dataset
-###############################################################################
+################################################################################
+# Generation of genotype dataset                                               #
+################################################################################
 
 
 def generate_genotype_dataset(output_directory, number_of_variable, number_of_patients, common_prefix, pattern_size, causal_genotype_SNPs):
-
+    # Initialisation of list to store SNPs IDs
     random_genotype_id = []
     causal_genotype_id = []
+    # Get causal_genotype_SNPs and transtype it in array
     linear_regression_genotype_dataset = np.array(causal_genotype_SNPs)
     # Check if output directory exist
     check_output_directory(output_directory)
@@ -164,9 +180,9 @@ def generate_genotype_dataset(output_directory, number_of_variable, number_of_pa
                np.r_[genotype_dataset], fmt='%s', delimiter=',')
 
 
-###############################################################################
-# Generation of phenotype dataset
-###############################################################################
+################################################################################
+# Generation of phenotype dataset                                              #
+################################################################################
 
 
 def generate_phenotype_dataset(output_directory, number_of_case, number_of_control, common_prefix, causal_phenotype_SNPs):
@@ -182,9 +198,9 @@ def generate_phenotype_dataset(output_directory, number_of_case, number_of_contr
                np.r_[final_matrix_causal_phenotype_SNPs], fmt='%s', delimiter=',')
 
 
-###############################################################################
-# Main function
-###############################################################################
+################################################################################
+# Main function                                                                #
+################################################################################
 
 
 def main():
@@ -218,6 +234,8 @@ def main():
             output_directory, number_of_case, number_of_control, common_prefix, causal_phenotype_SNPs)
 
 
-# Execution of main function
+################################################################################
+# Execution of main function                                                   #
+################################################################################
 if __name__ == "__main__":
     main()
