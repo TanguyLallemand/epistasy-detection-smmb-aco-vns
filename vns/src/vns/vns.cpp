3cd58fa3d2f70d87084fbc22c6a6bf9650f9f9fe
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-23 16:30:32 +0100

    implementation en pseudo code. To check

--- vns/src/vns/vns.cpp
+++ vns/src/vns/vns.cpp
@@ -2,7 +2,7 @@
 C'est un vnd un peu ameliore via une perturbation
 
 Selectionner une architecture de voisinnage N_k 1<=k<=k_max
-creer une solution intiale scanf("initialisation int main(int argc, char const *argv[]) {
+creer une solution intiale
     tant que non condition fin
         k<-1
         tant que k<k_max
@@ -14,3 +14,51 @@ creer une solution intiale scanf("initialisation int main(int argc, char const *
                 k++
         memoriser l'optimum local obtenue dans M
     retourner s, la meilleur solution rencontrer dans M*/
+
+/*
+function neighborhood_change(x,x',k)
+{
+    if f(x') < f(x)
+    {
+        x = x' //Make a move
+        k = 1 // Inital neighborhood
+    }
+    else
+    {
+        k = k+1 //Next neighborhood
+    }
+    return x,k
+}
+function Shake(x,k)
+{
+    w = (1+Rand(0,1) * N_k(s) )
+    x' = x^w
+    return x'
+}
+
+function VND(x, k_max)
+{
+    k = 1
+    while (k != kmax)
+    {
+        x' = argmin f(y) //Find the best neighbor in N_k(x)
+        x,k = neighborhood_change(x,x',k) //Change neighborhood
+    }
+    return x;
+}
+
+function general_VNS(x, l_max, k_max, t_max)
+{
+    while (t_max>t)
+    {
+        k = 1
+        while (k != k_max)
+        {
+            x' = shake(x,k)
+            x'' = VND(x',l_max)
+            x,k = neighborhood_change(x,x'',k)
+        }
+    }
+    return x;
+}
+*/
