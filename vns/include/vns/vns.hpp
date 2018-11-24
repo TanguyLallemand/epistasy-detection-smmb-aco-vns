310f40284f9e7ad24d94bd99119384e4a5794a3c
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-11-24 11:04:07 +0100

    progress

--- vns/include/vns/vns.hpp
+++ vns/include/vns/vns.hpp
@@ -1,2 +1,15 @@
-#ifndef VNS_H
-#define VNS_H
+#ifndef VNS_HPP
+#define VNS_HPP
+#include "parameters_parsing.hpp"
+
+
+class variable_neightborhood_search {
+
+public:
+    variable_neightborhood_search(parameters_parsing _params);
+private:
+    /* data */
+    int _n_it_max;
+    int _k_max;
+};
+#endif
