1112b0bf32605430ecba86c41e2e749d26c285e1
TanguyLallemand <tanguy.lallemand@gmail.com>
2018-11-24 11:03:54 +0100

    init for testing

--- vns/src/main.cpp
+++ vns/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
+#include "parameters_parsing.hpp"
+#include "vns.hpp"
 
 int main(int argc, char* argv[])
 {
-    
+    parameters_parsing params;
+    variable_neightborhood_search vns(params);
 }
