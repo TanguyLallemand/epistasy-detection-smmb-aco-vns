2322aafe44edda648f618ac30ab1098bf3b1c82c
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-29 20:57:38 +0100

    debug

--- launch_smmbaco_toy_example.py
+++ launch_smmbaco_toy_example.py
@@ -4,4 +4,4 @@ import subprocess
 # This programm require two parameters:
 # First: path of genotype dataset
 # Second: path of associated phenotype dataset
-subprocess.Popen('./smmb_aco/smmb_aco.exe ./toy_dataset/genotype_toy_dataset_simu_naive0.txt ./toy_dataset/phenotype_toy_dataset_simu_naive.txt', shell=True)
+subprocess.Popen('./smmb_aco/smmb_aco.exe ./toy_dataset/genotypes_toy_dataset.txt ./toy_dataset/phenotypes_toy_dataset.txt', shell=True)
