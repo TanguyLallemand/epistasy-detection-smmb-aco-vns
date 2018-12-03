d7e7fa787d114929323a1726509e94bf8b9806be
JonathanCruard <jonathan.cruard@gmail.com>
2018-12-03 15:12:18 +0100

    updated path

--- vns/toy_example/launch_vns_toy_example.py
+++ vns/toy_example/launch_vns_toy_example.py
@@ -8,4 +8,4 @@ import subprocess
 # This programm require two parameters:
 # First: path of genotype dataset
 # Second: path of associated phenotype dataset
-subprocess.Popen('./vns/vns.exe ./toy_dataset/genotypes_toy_dataset.txt ./toy_dataset/phenotypes_toy_dataset.txt', shell=True)
+subprocess.Popen('../vns.exe ./toy_dataset/genotypes_toy_dataset.txt ./toy_dataset/phenotypes_toy_dataset.txt', shell=True)
