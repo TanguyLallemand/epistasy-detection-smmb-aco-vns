0181aa4581523e7cbf2efd223a2c317b26b9d015
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-12-04 17:14:35 +0100

    more comments, seems to be final cut

--- smmb_aco/toy_example/launch_smmbaco_toy_example.py
+++ smmb_aco/toy_example/launch_smmbaco_toy_example.py
@@ -3,9 +3,9 @@
 # Authors: Tanguy Lallemand M2BB
 #          Jonathan Cruard M2BB
 import os
-import subprocess
 
-# This programm require two parameters:
-# First: path of genotype dataset
-# Second: path of associated phenotype dataset
+# This programm require three parameters:
+# First: path of genotype data set
+# Second: path of associated phenotype data set
+# Third: path to parameters file
 os.system('./smmb_aco/smmb_aco.exe ./smmb_aco/toy_example/toy_dataset/genotypes_toy_dataset.txt ./smmb_aco/toy_example/toy_dataset/phenotypes_toy_dataset.txt ./smmb_aco/toy_example/parameters.txt')
--- vns/toy_example/launch_vns_toy_example.py
+++ vns/toy_example/launch_vns_toy_example.py
@@ -3,10 +3,9 @@
 # Authors: Tanguy Lallemand M2BB
 #          Jonathan Cruard M2BB
 import os
-import subprocess
 
-# This programm require two parameters:
-# First: path of genotype dataset
-# Second: path of associated phenotype dataset
+# This programm require three parameters:
+# First: path of genotype data set
+# Second: path of associated phenotype data set
 # Third: path to parameters file
 os.system('./vns/vns.exe ./vns/toy_example/toy_dataset/genotypes_toy_dataset.txt ./vns/toy_example/toy_dataset/phenotypes_toy_dataset.txt ./vns/toy_example/parameters.txt')
