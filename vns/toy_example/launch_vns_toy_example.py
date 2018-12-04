1427173f8c87bcb11b6c719b6c3b0d6f9f89526e
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-12-04 13:29:50 +0100

    add comments

--- vns/toy_example/launch_vns_toy_example.py
+++ vns/toy_example/launch_vns_toy_example.py
@@ -8,4 +8,5 @@ import subprocess
 # This programm require two parameters:
 # First: path of genotype dataset
 # Second: path of associated phenotype dataset
+# Third: path to parameters file
 os.system('./vns/vns.exe ./vns/toy_example/toy_dataset/genotypes_toy_dataset.txt ./vns/toy_example/toy_dataset/phenotypes_toy_dataset.txt ./vns/toy_example/parameters.txt')
