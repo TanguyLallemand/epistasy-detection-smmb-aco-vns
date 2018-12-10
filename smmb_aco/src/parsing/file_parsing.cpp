e1881d4b5689eaef96bddb9df1c42dc1188fecf4
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-12-10 23:31:29 +0100

    comment file, need a bit more of comment

--- smmb_aco/src/parsing/file_parsing.cpp
+++ smmb_aco/src/parsing/file_parsing.cpp
@@ -1,12 +1,12 @@
 #include "file_parsing.hpp"
 
-//TODO voir si un destructeur est nécessaire
 
 //=============================================================================
-// data_parsing : constructeur
+// data_parsing : Constructor
 //=============================================================================
 data_parsing::data_parsing(string geno_filename, string pheno_filename, int header_size, char separator)
 {
+    // Get filenames given in argument
     _geno_filename = geno_filename;
     _pheno_filename = pheno_filename;
     _header_size = header_size;
@@ -25,21 +25,31 @@ data_parsing::data_parsing(string geno_filename, string pheno_filename, int head
 //=============================================================================
 void data_parsing::parse_geno()
 {
+    // Rezise object to right dimensions
     _geno_matrix.resize( _row_number, _col_number);
+    // Open file containning genos datas
     ifstream file(_geno_filename);
+    // Init variable to handle line
     string line;
+    // Get header data, storing SNPs names
     for (unsigned x = 0; x < _header_size; x++)
     {
         getline(file, line);
     }
+    // Iterate tought file containning geno datas
     for (size_t i = 0; i < _geno_matrix.size1(); i++)
     {
+        // Get line
         getline(file, line);
+        // Init an iterator
         int r=0;
+        // Iterate tought line
         for (size_t j = 0; j < line.size(); j++)
         {
+            // Get only numbers
             if (line[j]!=',')
             {
+                // Save in right cell data, -48 to transform string to int using ASCII format
                 _geno_matrix(i, r) = line[j]-48;
                 r++;
             }
@@ -52,16 +62,21 @@ void data_parsing::parse_geno()
 //=============================================================================
 void data_parsing::parse_pheno()
 {
+    // Resize vector following data set size
     _pheno_vector.resize(_row_number);
+    // Open file containning phenotype datas
     ifstream file(_pheno_filename);
     string line;
     for (size_t x = 0; x < _header_size; x++)
     {
+        // Get header
         getline(file, line);
     }
     for (size_t i = 0; i < _pheno_vector.size(); i++)
     {
+        // Get current line
         getline(file, line);
+        // Save in right cell data, -48 to transform string to int using ASCII format
         _pheno_vector(i) = line[0]-48;
     }
 }
@@ -76,7 +91,6 @@ void data_parsing::parse_snp_id()
     string line;
     getline(file, line);
     int i=0;
-
     for (size_t r = 0; r < line.size(); r++)
     {
         if (line[r]==_separator)
@@ -94,17 +108,20 @@ void data_parsing::parse_snp_id()
 //=================================================
 void data_parsing::get_line_nb()
 {
+    // Open file containning phenotype datas
     ifstream file(_pheno_filename);
     string temp;
     getline(file, temp);
     _row_number=0;
+    // Check if file is opened
     if (file)
     {
+        // Iterate tought line
         while (getline(file, temp))
         {
+            // Increment row count
             _row_number++;
         }
-        //_row_number = _row_number - _header_size;
     }
     else
     {
@@ -117,17 +134,28 @@ void data_parsing::get_line_nb()
 //=================================================
 void data_parsing::get_col_nb()
 {
+    // Open file containning genotype datas
     ifstream file(_geno_filename);
     string line;
     int i;
     _col_number=1;
     getline(file, line);
     int lenght = line.size();
-    for (i = 0; i < lenght; i++)
+    // Check if file is opened
+    if (file)
     {
-        if (line[i]==_separator)
+        // Iterate tought line
+        for (i = 0; i < lenght; i++)
         {
-            _col_number++;
+            if (line[i]==_separator)
+            {
+                // Increment column count
+                _col_number++;
+            }
         }
+    }
+    else
+    {
+        cout << "unable to open : "<< _geno_filename << '\n';
     }
 }
