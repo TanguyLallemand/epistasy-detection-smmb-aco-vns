6026c30cbffbb49ab92edb392d6ba4b51cd7ef62
TanguyLallemand <tanguy.lallemand@gmail.com>
2019-01-03 19:10:07 +0100

    still progress
    Former-commit-id: 26ec5cf98d97a06fe0dc98fb563a7933c0880058

--- gametes_datas/script_parsing_gametes_datas.sh
+++ gametes_datas/script_parsing_gametes_datas.sh
@@ -9,11 +9,15 @@ for first_dir in "$(ls -d */ | cut -f1 -d'/')"; do
         for second_dir in "$(ls -d */ | cut -f1 -d'/')"; do
             for sub_directory in $second_dir; do
                 cd "$sub_directory"
-                for file in "$(ls)"; do
+                for file in *.txt ; do
+                    # Get basename
+                    # basename=$(echo "$filename" | cut -f 1 -d '.')
                     # Replace tab by commas
                     sed 's/\t/,/g' "$file" > genotype_"$file".csv
                     # Get last column phenotype column
-                    cat genotype_"$file".csv | awk -F, '{print $NF}' > phenotype_"$file".csv
+                    cat genotype_"$file".csv | awk -F',' '{print $NF}' > phenotype_"$file".csv
+                    # cat genotype_"$file".csv | awk -F',' '{print $NF}' > genotype_"$file".csv
+                    # awk -F',' 'NF{NF-=1};1' <genotype_"$file".csv >genotype_"$file".csv
                 done
                 cd ..
             done
