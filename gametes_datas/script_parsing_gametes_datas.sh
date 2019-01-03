e4917d37384e20a1c5d0648cd0bd6bbf7ed30bf6
TanguyLallemand <tanguy.lallemand@gmail.com>
2019-01-03 21:55:00 +0100

    add comments
    Former-commit-id: 74b27e4b95f0b62dd831e8d2ea2224e7887c0a04

--- gametes_datas/script_parsing_gametes_datas.sh
+++ gametes_datas/script_parsing_gametes_datas.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-#extraction
+# Extract raw datas
 tar xzvf repository.tar.gz  -C ./datas
+# Go in folder containning datas
 cd datas/repository
+# Loop on first level of directories
 for first_dir in "$(ls -d */ | cut -f1 -d'/')"; do
     for directory in $first_dir; do
         cd "$directory"
+        # Loop on subdrirectories
         for second_dir in "$(ls -d */ | cut -f1 -d'/')"; do
             for sub_directory in $second_dir; do
                 cd "$sub_directory"
+                # Loop on files
                 for file in *.txt ; do
                     # Get basename
                     basename=${file%.txt}
@@ -16,8 +20,10 @@ for first_dir in "$(ls -d */ | cut -f1 -d'/')"; do
                     sed 's/\t/,/g' "$file" > temp_"$basename".csv
                     # Get last column phenotype column
                     cat temp_"$basename".csv | awk -F',' '{print $NF}' > phenotype_"$basename".csv
+                    # Delete last column in genotype data
                     cat temp_"$basename".csv | awk -F',' '{OFS=","};NF{NF-=1};1' <temp_"$basename".csv >genotype_"$basename".csv
                 done
+                # Remove raw datas
                 rm *.txt
                 cd ..
             done
