e4cda62e802a05f093d57b210c89e403383a53c1
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-01-07 17:34:32 +0100

    add comments, few tweaks
    Former-commit-id: 32d916658d062e5fd5724373338028c445e6125a

--- vns/src/main.cpp
+++ vns/src/main.cpp
@@ -14,20 +14,22 @@
 
 int main(int argc, char* argv[])
 {
-    // Arguments
+    // Get arguments passed to programm
 	std::string genos_file = argv[1];
 	std::string phenos_file = argv[2];
 	std::string parameters_file = argv[3];
-
+	// Parse parameters
     parameters_parsing params(parameters_file);
 	params.genos_file = genos_file;
 	params.phenos_file = phenos_file;
 	int header = params.header;
 	char separator = params.separator;
-
+	// Parse data
 	data_parsing data(genos_file, phenos_file, header, separator);
-	vns test(data, params);
-	test.run();
-
+	// Instanciate vns class
+	vns vns_instance(data, params);
+	// Launch algorithm
+	vns_instance.run();
+	// End programm
 	return 0;
 }
