7e64c5c200c92a949fad48c081ccea4a58db2434
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-01-16 11:36:43 +0100

    add a log file
    Former-commit-id: 906c330a42e37c3e3d39ecc51486b381fb7dcdee

--- launch_vns_toy_example.py
+++ launch_vns_toy_example.py
@@ -12,5 +12,6 @@
 # First: path of genotype data set
 # Second: path of associated phenotype data set
 # Third: path to parameters file
+# Finally > is used to redirect standard output in a log file
 import os
-os.system('./vns/vns.exe ./gametes_datas/datas/repository/model1/model1_0_01p_0005h_005m/genotype_model1_0_01p_0005h_005m_005.csv ./gametes_datas/datas/repository/model1/model1_0_01p_0005h_005m/phenotype_model1_0_01p_0005h_005m_005.csv ./vns/toy_example/parameters.txt')
+os.system('./vns/vns.exe ./gametes_datas/datas/repository/model1/model1_0_01p_0005h_005m/genotype_model1_0_01p_0005h_005m_005.csv ./gametes_datas/datas/repository/model1/model1_0_01p_0005h_005m/phenotype_model1_0_01p_0005h_005m_005.csv ./vns/toy_example/parameters.txt > ./vns/outputs/log.log')
