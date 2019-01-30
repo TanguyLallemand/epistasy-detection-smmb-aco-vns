57e09afcc027eb9f905f423c054660d1deeaf06f
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-01-30 20:48:47 +0100

    launcher to evaluate same files with both methods
    Former-commit-id: 918a18d7198fd915183a10923fd690e3c6270e5a

--- launch_evaluation.py
+++ launch_evaluation.py
@@ -9,7 +9,8 @@
 ################################################################################
 #
 #
-# usage: eval_simu.py [-h] -i INPUT -o OUTPUT [-n NRUNS] -m METHOD
+# usage: eval_simu.py [-h] -i INPUT -o OUTPUT [-l CAUSAL_ID] -m METHOD
+#                     [-n NRUNS] [-nf NFILES]
 #
 # optional arguments:
 #   -h, --help            show this help message and exit
@@ -17,10 +18,16 @@
 #                         Give an input directory
 #   -o OUTPUT, --output OUTPUT
 #                         Give an output directory
+#   -l CAUSAL_ID, --causal_id CAUSAL_ID
+#                         Letter to identify causal SNPs
+#   -m METHOD, --method METHOD
+#                         Method to test
 #   -n NRUNS, --nruns NRUNS
 #                         Number of method executions to be performed on each
 #                         file in the dataset
-#   -m METHOD, --method METHOD
-#                         Method to test
+#   -nf NFILES, --nfiles NFILES
+#                         Number of file to test in the dataset
+
 import os
 os.system('./evaluation/eval_simu.py -i ./gametes_datas/datas/repository/model1/model1_0_01p_0005h_005m/ -o ./evaluation/result_eval/ -n 100 -m vns -nf 10 -l M')
+os.system('./evaluation/eval_simu.py -i ./gametes_datas/datas/repository/model1/model1_0_01p_0005h_005m/ -o ./evaluation/result_eval/ -n 100 -m smmb -nf 10 -l M')
