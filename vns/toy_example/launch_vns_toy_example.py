73dc1d1103baa9709e2196a2a5f53d539bae4413
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-06 13:29:03 +0100

    add some comments

--- vns/toy_example/launch_vns_toy_example.py
+++ vns/toy_example/launch_vns_toy_example.py
@@ -2,10 +2,18 @@
 # -*- coding: utf-8 -*-
 # Authors: Tanguy Lallemand M2BB
 #          Jonathan Cruard M2BB
-import os
-
-# This programm require three parameters:
+#
+#
+# This script given as an example of launcher for VNS method
+#
+################################################################################
+# Arguments                                                                    #
+################################################################################
+#
+#
 # First: path of genotype data set
 # Second: path of associated phenotype data set
 # Third: path to parameters file
+# Finally > is used to redirect standard output in a log file
+import os
 os.system('./vns/vns.exe ./vns/toy_example/toy_dataset/genotypes_toy_dataset.txt ./vns/toy_example/toy_dataset/phenotypes_toy_dataset.txt ./vns/toy_example/parameters.txt')
