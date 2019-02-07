6ba3d58cb35e802c67d778a0b5a85d9790e59024
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-07 21:14:46 +0100

    add basic call example, purge some files

--- launch_evaluation.py
+++ launch_evaluation.py
@@ -11,7 +11,7 @@
 #
 # usage: eval_simu.py [-h] -i INPUT -o OUTPUT [-l CAUSAL_ID] -m METHOD
 #                     [-n NRUNS] [-nf NFILES]
-# 
+#
 # optional arguments:
 #   -h, --help            show this help message and exit
 #   -i INPUT, --input INPUT
@@ -28,7 +28,8 @@
 #   -nf NFILES, --nfiles NFILES
 #                         Number of file to test in the dataset
 
-
+# An example of basic call:
+# os.system('./evaluation/eval_simu.py -i ./Simu_naive/Simu_naive_2snp_0.25/ -o ./evaluation/result_eval/ -n 50 -m smmb_aco -nf 20')
 import os
 
 os.system('./evaluation/eval_simu.py -i ./data_simulated_from_class/simu_naive_CARLUER_OUEDRAOGO/simu_naive_2snp_0_059_0_25/ -o ./evaluation/result_eval/ -n 100 -m vns -nf 100 -l M')
