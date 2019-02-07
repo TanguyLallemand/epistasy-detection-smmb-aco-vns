7be8319a8f902c3fbcd4defd7d68a0f3f8c57a4a
tanguy.lallemand <tanguy.lallemand@gmail.com>
2019-02-07 21:12:42 +0100

    rework gamete datas parsing

--- script_parsing_gametes_datas.sh
+++ script_parsing_gametes_datas.sh
@@ -4,11 +4,11 @@
 #
 # This script must be run next to the repository.tar.gz archive that collects gamete data.
 # It will then decompress the archive and reformat data so that they can be used directly by our programs.
-mkdir datas
+mkdir ./data_simulated_from_class/gametes_datas/datas
 # Extract raw datas
-tar xzvf repository.tar.gz  -C ./datas
+tar xzvf ./data_simulated_from_class/gametes_datas/repository.tar.gz  -C ./data_simulated_from_class/gametes_datas//datas
 # Go in folder containning datas
-cd datas/repository
+cd ./data_simulated_from_class/gametes_datas/datas/repository
 echo -e '\033[33mParse files to be suitable with input of methods \033[0m'
 # Loop on first level of directories
 for first_dir in "$(ls -d */ | cut -f1 -d'/')"; do
